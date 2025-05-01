586a7638cd335b958cae9c818e50dd297bccfa9a
heejjinkim <hjhj12@dgu.ac.kr>
2025-05-01 16:26:41 +0900

    feat: 예외 응답 통일

---
+++ src/main/java/com/Ureka/AnalDoo/common/exception/RestApiException.java
@@ -0,0 +1,12 @@
+package com.Ureka.AnalDoo.common.exception;
+
+import com.Ureka.AnalDoo.common.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RestApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
