c868c09761d3e2b4d03377d046ebb8bdc8690f5f
Jinu Yi <dionisos198@naver.com>
2025-05-01 14:17:42 +0900

    Setting: 프로젝트 초기 세팅

---
+++ src/main/java/com/Ureka/AnalDoo/AnalDooApplication.java
@@ -0,0 +1,14 @@
+package com.Ureka.AnalDoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalDooApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AnalDooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ureka/AnalDoo/AnalDooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ureka.AnalDoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalDooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
