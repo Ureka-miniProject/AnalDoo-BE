2da030643abf5220e4f9ed65ada8639a723a5f25
Jinu Yi <dionisos198@naver.com>
2025-05-05 19:28:08 +0900

    feat: 결제 환경 세팅

---
+++ src/main/java/com/Ureka/AnalDoo/domain/payment/config/IamPortClientConfig.java
@@ -0,0 +1,25 @@
+package com.Ureka.AnalDoo.domain.payment.config;
+
+import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IamPortClientConfig {
+
+    @Value("${portOne.apiKey}")
+    private String apiKey;
+
+    @Value("${portOne.apiSecret}")
+    private String apiSecret;
+
+
+    @Bean
+    public IamportClient iamportClient(){
+
+        IamportClient iamportClient = new IamportClient(apiKey,apiSecret);
+
+        return iamportClient;
+    }
+}
