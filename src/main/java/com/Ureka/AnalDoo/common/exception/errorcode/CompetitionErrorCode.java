adcafa763af235df44c75e7b8a2de5e1b6bd67cb
heejjinkim <hjhj12@dgu.ac.kr>
2025-05-07 23:53:09 +0900

    feat: CompetitionErrorCode 추가

---
+++ src/main/java/com/Ureka/AnalDoo/common/exception/errorcode/CompetitionErrorCode.java
@@ -0,0 +1,17 @@
+package com.Ureka.AnalDoo.common.exception.errorcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CompetitionErrorCode implements ErrorCode {
+
+    COMPETITION_NOT_FOUND(HttpStatus.NOT_FOUND, "대회를 찾을 수 없습니다."),
+    COMPETITION_CLOSED(HttpStatus.BAD_REQUEST, "이미 모집이 마감된 대회입니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
