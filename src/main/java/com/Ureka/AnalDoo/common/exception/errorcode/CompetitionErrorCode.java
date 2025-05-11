cfe082bdf521eec6445c364aee9c3d1b855f7cbd
Jinu Yi <dionisos198@naver.com>
2025-05-11 23:32:14 +0900

    feat: 대회 취소 기능 및 환불

--- src/main/java/com/Ureka/AnalDoo/common/exception/errorcode/CompetitionErrorCode.java
+++ src/main/java/com/Ureka/AnalDoo/common/exception/errorcode/CompetitionErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum CompetitionErrorCode implements ErrorCode {
 
     COMPETITION_NOT_FOUND(HttpStatus.NOT_FOUND, "대회를 찾을 수 없습니다."),
-    ;
+    COMPETITION_USER_NOT_MATCH(HttpStatus.BAD_REQUEST,"대회 주최자가 아닙니다");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/Ureka/AnalDoo/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/Ureka/AnalDoo/domain/reservation/repository/ReservationRepository.java
@@ -5,8 +5,10 @@ import com.Ureka.AnalDoo.common.exception.errorcode.CompetitionErrorCode;
 import com.Ureka.AnalDoo.common.exception.errorcode.ReservationErrorCode;
 import com.Ureka.AnalDoo.domain.entity.Competition;
 import com.Ureka.AnalDoo.domain.entity.Reservation;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,6 +16,10 @@ public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
 
     Optional<Reservation> findByIdAndIsDeleted(Long id,boolean isDeleted);
+
+    @Query("select r from Reservation r where r.competition.id =:competitionId and r.isDeleted = false")
+    List<Reservation> findByCompetitionId(Long competitionId);
+
 
     default Reservation getById(Long id) {
         return findByIdAndIsDeleted(id,false).orElseThrow(() -> new RestApiException(ReservationErrorCode.RESERVATION_NOT_FOUND));
