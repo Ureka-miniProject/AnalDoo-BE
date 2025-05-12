cf360f1b5562fe2a80db9aab8aab5bb50964b832
= <hjhj12@dgu.ac.kr>
2025-05-12 18:12:23 +0900

    fix: request 기본 생성자 추가

--- src/main/java/com/Ureka/AnalDoo/domain/reservation/dto/request/ReservationCreateRequest.java
+++ src/main/java/com/Ureka/AnalDoo/domain/reservation/dto/request/ReservationCreateRequest.java
@@ -3,9 +3,11 @@ package com.Ureka.AnalDoo.domain.reservation.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReservationCreateRequest {
 
     @NotNull
