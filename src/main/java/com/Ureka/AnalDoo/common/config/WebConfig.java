9224c025761b4ca4bb5806efb6e972a63c3cb8bc
Jinu Yi <dionisos198@naver.com>
2025-05-14 08:59:56 +0900

    refactor: cors 에러 해결

--- src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
+++ src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
@@ -8,8 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+        registry.addMapping("/api/v1/**") // cors를 적용할 spring서버의 url 패턴.
+                .allowedOrigins(
+                        "http://localhost:3000","https://anal-doo-fe.vercel.app","http://anal-doo-fe.vercel.app") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedMethods("GET", "POST", "PUT", "PATCH",
+                        "DELETE", "OPTIONS") // cors를 허용할 method + DELETE 추가
+                .allowedHeaders("Content-Type", "Authorization","accessToken","refreshToken")
+                .exposedHeaders("Authorization","Set-Cookie")
+                .allowCredentials(true);
     }
 }
