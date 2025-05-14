c2674d2ff40022be045b8452decf86fc23511e43
Jinu Yi <dionisos198@naver.com>
2025-05-14 09:16:21 +0900

    refactor: 결제 코드 수정 롤백

--- src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
+++ src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
@@ -11,10 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/v1/**") // cors를 적용할 spring서버의 url 패턴.
                 .allowedOrigins(
                         "http://localhost:3000","https://anal-doo-fe.vercel.app","http://anal-doo-fe.vercel.app") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
-                .allowedMethods("GET", "POST", "PUT", "PATCH",
-                        "DELETE", "OPTIONS") // cors를 허용할 method + DELETE 추가
-                .allowedHeaders("Content-Type", "Authorization","accessToken","refreshToken")
-                .exposedHeaders("Authorization","Set-Cookie")
+                .allowedMethods("*") // cors를 허용할 method + DELETE 추가
+                .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowCredentials(true);
     }
 }
