a41c74b350d1f4f2b20ca8db87acf6aee48eea1c
Jinu Yi <dionisos198@naver.com>
2025-05-14 09:21:31 +0900

    refactor: cors 에러 해결

--- src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
+++ src/main/java/com/Ureka/AnalDoo/common/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*") // cors를 허용할 method + DELETE 추가
                 .allowedHeaders("*")
                 .exposedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
