aae8a0e49786e260d16d5bf3c56e5105c38f6459
= <hjhj12@dgu.ac.kr>
2025-05-14 10:21:51 +0900

    fix: CORS 설정 수정

--- src/main/java/com/Ureka/AnalDoo/auth/config/SecurityConfig.java
+++ src/main/java/com/Ureka/AnalDoo/auth/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.Ureka.AnalDoo.auth.jwt.JWTUtil;
 import com.Ureka.AnalDoo.auth.jwt.LoginFilter;
 import com.Ureka.AnalDoo.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Collections;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -53,13 +55,17 @@ public class SecurityConfig {
 
                                 CorsConfiguration configuration = new CorsConfiguration();
 
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowedOrigins(List.of(
+                                        "http://localhost:3000",
+                                        "https://anal-doo-fe.vercel.app"
+                                ));
+                                configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                                configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                                 configuration.setAllowCredentials(true);
-                                configuration.setAllowedHeaders(Collections.singletonList("*"));
                                 configuration.setMaxAge(3600L);
 
-
+                                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                                source.registerCorsConfiguration("/**", configuration);
                                 return configuration;
                             }
                         })
